65f93a933408c50a76ad1a2bd5adcae2665aba0d
ecanon <ecanon@bancodeoccidente.com.co>
2025-09-26 00:55:07 -0500

    Deploy, whit Repo route

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Presentacion_2025/',
   server: {
     port: 3000, // Set your desired port here
   },
+  build: {
+    outDir: 'dist'
+  }
 })
