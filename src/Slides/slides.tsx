08b0ab6c566597047aed855c8dd7b30cd7eec9c3
ecanon <ecanon@bancodeoccidente.com.co>
2025-09-27 14:50:03 -0500

    Revision de españa

--- src/Slides/slides.tsx
+++ src/Slides/slides.tsx
@@ -39,12 +39,12 @@ export const Slide_2 = (props) => {
 const SLIDES_INFO = [
     {
         "title": "Importancia del pan en el Mundo",
-        "content": "El pan es un alimento básico y símbolo cultural en España, presente en todas las comidas y tradiciones. Su relevancia histórica y social lo convierte en parte esencial de la identidad nacional.",
+        "content": "El pan es un alimento básico y símbolo cultural, presente en todas las comidas y tradiciones. Su relevancia histórica y social lo convierte en parte esencial de la identidad nacional.",
         "URL": "https://wallpapers.com/images/featured/pan-rtzqhi1ok4k1bxlo.jpg"
     },
     {
         "title": "Historia y gremios",
-        "content": "En la Edad Media, los panaderos se organizaban en gremios y la producción era regulada por concejos municipales, mostrando el control y valor social del pan en la economía española.",
+        "content": "En la Edad Media, los panaderos se organizaban en gremios y la producción era regulada por concejos municipales, mostrando el control y valor social del pan en la economía.",
         "URL": "https://media.istockphoto.com/id/475263838/es/foto/muchos-mezcl%C3%B3-panes-y-bollos-toma-realizada-desde-arriba.jpg?s=612x612&w=0&k=20&c=KmBEZnxZhaajwHukLR9SblDBQlMvXWKCsPE3WsC0z4Y="
     },
     {
@@ -54,12 +54,12 @@ const SLIDES_INFO = [
     },
     {
         "title": "Variedad regional",
-        "content": "Cada región de España tiene su especialidad de pan, como el 'pa de pagès' en Cataluña y el pan de centeno en Galicia, reflejando la diversidad cultural y gastronómica del país.",
+        "content": "Por ejemplo en cada región de España tiene su especialidad de pan, como el 'pa de pagès' en Cataluña y el pan de centeno en Galicia, reflejando la diversidad cultural y gastronómica del país.",
         "URL": "https://png.pngtree.com/background/20250816/original/pngtree-milky-breads-hd-8k-wallpaper-stock-photographic-image-artisan-bread-display-picture-image_15818302.jpg"
     },
     {
         "title": "Significado simbólico",
-        "content": "El pan simboliza generosidad y hospitalidad en la sociedad española, siendo un alimento que une a la comunidad y representa valores compartidos.",
+        "content": "El pan simboliza generosidad y hospitalidad en la sociedad, siendo un alimento que une a la comunidad y representa valores compartidos.",
         "URL": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3ed2541d-106f-444e-ac42-51e460b58ac1/dkkafmb-19ae3640-1df2-4544-851b-42316a0c84c9.jpg/v1/fill/w_894,h_894,q_70,strp/banana_bread__kiwis_and_corn_by_tokomokoshoko_dkkafmb-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyNCIsInBhdGgiOiIvZi8zZWQyNTQxZC0xMDZmLTQ0NGUtYWM0Mi01MWU0NjBiNThhYzEvZGtrYWZtYi0xOWFlMzY0MC0xZGYyLTQ1NDQtODUxYi00MjMxNmEwYzg0YzkuanBnIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.KIEa5dR6BQYBZOQKgN2WKtum4T6Qlo7Y6mdLHF7m_hU"
     },
     {
@@ -69,12 +69,12 @@ const SLIDES_INFO = [
     },
     {
         "title": "Calidad y tradición",
-        "content": "El aumento de costos y competencia ha afectado la calidad del pan en algunas áreas, pero sigue siendo fundamental en la cultura y tradición españolas.",
+        "content": "El aumento de costos y competencia ha afectado la calidad del pan en algunas áreas, pero sigue siendo fundamental en la cultura y tradición mundial.",
         "URL": "https://i.pinimg.com/736x/cc/1e/72/cc1e72b1045ee2aca015f6f4dfa53b88.jpg"
     },
     {
         "title": "El pan en la dieta diaria",
-        "content": "El pan se consume diariamente en la mayoría de los hogares españoles, manteniéndose como parte esencial de la alimentación y costumbres familiares.",
+        "content": "El pan se consume diariamente en la mayoría de los hogares del mundo, manteniéndose como parte esencial de la alimentación y costumbres familiares.",
         "URL": "https://i.pinimg.com/1200x/b8/d6/f1/b8d6f19c86aabf4a90c024832b0c0efe.jpg"
     },
     {
